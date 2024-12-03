f4caa6a4ef560071690f2456a12703ebaf9858d1
tim-oe <tecronin@gmail.com>
2024-12-02 23:27:52 -0600

    sdm updates added shutter

--- src/sdm/image.sh
+++ src/sdm/image.sh
@@ -1,6 +1,6 @@
 # https://github.com/gitbls/sdm/blob/master/Docs/Command-Details.md
 # https://github.com/gitbls/sdm/blob/master/Docs/Plugins.md
-# keep script for when needing to run interactive
+# TODO https://github.com/gitbls/sdm/blob/master/Docs/Plugins.md#gadgetmode
 # serial is not working right
 #
 
@@ -13,16 +13,14 @@ sudo sdm --customize \
 --plugin user:"deluser=pi" \
 --plugin user:"adduser=tcronin" \
 --plugin user:"setpassword=tcronin|password=$TEC_PWD" \
---plugin mkdir:"dir=/home/tcronin/.ssh|chown=tcronin:tcronin|chmod=700" \
---plugin copyfile:"from=/mnt/clones/data/home/tcronin/.ssh/authorized_keys|to=/home/tcronin/.ssh|runphase=postinstall|chown=tcronin:tcronin|chmod=600" \
+--plugin sshkey:"sshuser=tcronin|keyname=authorized_keys|import-key=/mnt/clones/data/home/tcronin/.ssh/authorized_keys" \
 --plugin copyfile:"from=src/home/.bash_aliases|to=/home/tcronin|runphase=postinstall|chown=tcronin:tcronin|chmod=644" \
 --plugin user:"adduser=ansible" \
 --plugin user:"setpassword=ansible|password=$ANS_PWD" \
---plugin mkdir:"dir=/home/ansible/.ssh|chown=ansible:ansible|chmod=700" \
---plugin copyfile:"from=/mnt/clones/data/home/ansible/.ssh/authorized_keys|to=/home/ansible/.ssh|runphase=postinstall|chown=ansible:ansible|chmod=600" \
+--plugin sshkey:"sshuser=ansible|keyname=authorized_keys|import-key=/mnt/clones/data/home/ansible/.ssh/authorized_keys" \
 --plugin copyfile:"from=src/etc/profile.d/motd.sh|to=/etc/profile.d|runphase=postinstall|chown=root:root|chmod=644" \
 --plugin L10n:"keymap=us|locale=en_US.UTF-8|timezone=America/Chicago" \
---plugin disables:"triggerhappy|piwiz" \
+--plugin disables:"bluetooth|triggerhappy|piwiz" \
 --plugin network:"netman=nm" \
 --extend --xmb 4096 \
 --regen-ssh-host-keys \
