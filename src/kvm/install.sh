0acce0713ac582626386c261d40fbca36f3d4890
tim-oe <tecronin@gmail.com>
2025-03-04 22:31:08 -0600

    full kvm install with bridged network removed redundent root user

---
+++ src/kvm/install.sh
@@ -0,0 +1,12 @@
+virt-install \
+	--name _VM_NAME_\
+	--description '_VM_DESC_' \ 
+	--memory 4096 \ 
+	--vcpus 1 \ 
+	--disk _VM_DISK_,bus=sata \
+	--import \
+	--os-variant _VM_OS_ \
+	--virt-type kvm \
+	--autostart \
+	--noautoconsole \
+	--network network=host-bridge
